bf8af3d86d3d7d95b927c00825d4acf74c5af58c
Zenkryz <Zenkryz@mail.ru>
2024-04-09 10:46:18 +0500

    Добавил ввод числа и вывод его в квадрате

--- Seminars/Sem1/Program.cs
+++ Seminars/Sem1/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(a*a);
