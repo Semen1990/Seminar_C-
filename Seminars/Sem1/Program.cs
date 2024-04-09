4fd15d6cd7fdba3a19143c2d98cc50f07d74d41e
Zenkryz <Zenkryz@mail.ru>
2024-04-09 15:46:02 +0500

    Сохранил последние изменения

--- Seminars/Sem1/Program.cs
+++ Seminars/Sem1/Program.cs
@@ -27,16 +27,16 @@
 // }
 
 
-// Console.WriteLine("Введите трехзначное число: ");
-// int n = Convert.ToInt32(Console.ReadLine());
-// if (n >= 100 && n <= 1000)
-// {
-//    int fn = n % 10;
-//    int ln = n/100;
-//    Console.Write($"Сумма первго числа {fn} и последнего числа {ln} трехзначного числа =  {fn + ln}");
-// }
-// else
-// {
-//    Console.Write("Вы ввели не трехзначное число");
-// }
+Console.WriteLine("Введите трехзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n >= 100 && n <= 1000)
+{
+   int fn = n % 10;
+   int ln = n/100;
+   Console.Write($"Сумма первго числа {fn} и последнего числа {ln} трехзначного числа =  {fn + ln}");
+}
+else
+{
+   Console.Write("Вы ввели не трехзначное число");
+}
 
