1f2aeefb34a5c861fcd2fcbcde676460ff664ffc
Zenkryz <Zenkryz@mail.ru>
2024-04-09 11:15:17 +0500

    Решил первую задачу

--- Seminars/Sem1/Program.cs
+++ Seminars/Sem1/Program.cs
@@ -1,4 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Введите число: ");
+// Console.WriteLine("Введите число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine($"Квадрат вашего числа {a} =  {a*a}");
+
+Console.WriteLine("Введите первое число: ");
 int a = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine(a*a);
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a == b*b)
+{
+System.Console.WriteLine($"Введеное число {a} является квадратом числа {b}");
+}
+else
+{
+   System.Console.WriteLine($"Введеное число {a} не является квадратом числа {b}"); 
+}
