6bae961ae2e2c5ad9abcfbdac95c1dfb1d4fac0d
Zenkryz <Zenkryz@mail.ru>
2024-04-16 11:47:43 +0500

    Первая задача второго семинара

---
+++ Seminars/Sem2/Program.cs
@@ -0,0 +1,17 @@
+﻿int DeleteSd (int num)
+{
+    if (num >= 100 && num <= 1000)
+ {
+   int ed = num%10;
+   int sot = num /100;
+   return sot * 10 + ed;
+ }
+else
+ {
+   Console.Write("Вы ввели не трехзначное число");
+   return 0;
+ }
+}
+Console.WriteLine("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(DeleteSd(num));
